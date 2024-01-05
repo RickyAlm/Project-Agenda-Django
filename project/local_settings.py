dd6f9c91ab374ff74849fb491e87c20192a28e0d
Henrique Almeida <rick.vieira.almeida@gmail.com>
2024-01-05 15:58:24 -0300

    Inserindo a base de dados, pasta media e local_settings.py

---
+++ project/local_settings.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'django-insecure-_3bx#g3=1rr-f$6)apg*z3(_x8dffg)rzcm%ff8a8uw-d$c&f6'  # noqa: E501
+DEBUG = True
+ALLOWED_HOSTS: list[str] = []
--- project/settings.py
+++ project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3bx#g3=1rr-f$6)apg*z3(_x8dffg)rzcm%ff8a8uw-d$c&f6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ''
 
 ALLOWED_HOSTS = []
 
